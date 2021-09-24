89a9936395a1d385fb53295b419ceae8873bbcf0
Usman Tahir <usman.tahir@tenovos.com>
2021-09-25 00:00:40 +0200

    added small api support

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask_restful import Api, reqparse
 
 def create_app():
   app = Flask(__name__)
   app.config['SCRIPT_KEY'] = 'random string'
+  api = Api(app)
 
   from .views import views
   from .auth import auth
+  from .api.auth import Auth
 
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
+
+  api.add_resource(Auth, '/api/auth/<int:user_id>')
 
   return app
---
+++ website/api/auth.py
@@ -0,0 +1,47 @@
+from flask import request
+from flask_restful import Resource, reqparse, abort
+
+users = [
+  {
+    "id": 1,
+    "name": "Usman"
+  },
+  {
+    "id": 2,
+    "name": "Tahir"
+  }
+]
+
+def get_item(id):
+  user = None
+  for u in users:
+    if (u['id'] == id):
+      user = u
+  return user
+
+class Auth(Resource):
+  def get(self, user_id):
+    user = get_item(user_id)
+    if (user != None):
+      return user
+    else:
+      abort(404, message='id Not valid')
+
+  def post(self, user_id):
+    # Define Body
+    user_post_args = reqparse.RequestParser()
+    user_post_args.add_argument("name", type=str, help="Name of Item", required=True)
+
+    # Assign arguments to request
+    args = user_post_args.parse_args()
+    return {
+      "data": "Posted",
+      "name": user_id,
+      "args": args
+    }
+
+  def put(self, id):
+    print(request.form)
+    return {
+      "data": "Put"
+    }
